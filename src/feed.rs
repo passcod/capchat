f488ee3edfe03e0f1bff631a86b74b8341a25adb
Félix Saparelli <felix@passcod.name>
2021-09-15 23:03:30 +1200

    Sketch support for more feed types

--- src/feed.rs
+++ src/feed.rs
@@ -2,38 +2,40 @@ use std::mem::take;
 
 use color_eyre::eyre::{eyre, Result};
 use futures::future::try_join_all;
+use mime::Mime;
 use serde::Deserialize;
 use sled::Tree;
-use tracing::{debug, error, info, trace};
+use tracing::{debug, info, trace};
 
 use crate::cap::{fetch_cap, Cap};
 
 pub async fn fetch_feed(cache: Tree, url: String) -> Result<Vec<Cap>> {
 	info!(%url, "fetching CAP feed");
-	let resp = reqwest::get(&url).await?;
+	let resp = reqwest::get(&url).await?.error_for_status()?;
 
-	if !resp.status().is_success() {
-		error!(status=%resp.status(), "failed to fetch feed");
-		return Err(eyre!("failed to fetch feed {}", url));
-	}
+	let content_type = resp.headers().get("content-type").map(|v| v.to_str()).transpose()?.unwrap_or("?");
 
 	info!(
 		%url,
 		age=%resp.headers().get("age").map(|v| v.to_str()).transpose()?.unwrap_or("?"),
-		bytes=%resp.headers().get("content-length").map(|v| v.to_str()).transpose()?.unwrap_or("?"),
+		bytes=%resp.content_length().unwrap_or(0),
+		%content_type,
 		"got CAP feed"
 	);
 	trace!(status=?resp.status(), headers=?resp.headers(), "headers");
+
+	let media_type: Mime = content_type.parse()?;
+	trace!(%url, ?media_type, "decoded media type");
 
 	let body = resp.text().await?;
 	debug!(%url, chars=%body.len(), "decoded body as text");
 	trace!(%url, body=%body, "decoded body");
 
-	let mut rss: Rss = serde_xml_rs::from_str(&body)?;
-	trace!(%url, ?rss, "parsed rss");
-
-	let items = take(&mut rss.channel.items);
-	info!(%url, ?rss, "found {} items in rss", items.len());
+	let items = match (media_type.type_(), media_type.subtype().as_str(), media_type.suffix().map(|s| s.as_str())) {
+		(mime::APPLICATION, "atom", Some("xml")) => todo!("atom support"),
+		(mime::APPLICATION, "rss", Some("xml")) => parse_rss(&url, &body)?,
+		_ => return Err(eyre!("unsupported media type: {}", media_type)),
+	};
 
 	let mut new = Vec::with_capacity(items.len());
 	for item in items {
@@ -67,6 +69,16 @@ pub async fn fetch_feed(cache: Tree, url: String) -> Result<Vec<Cap>> {
 	.collect::<Result<_, _>>()?;
 
 	Ok(caps)
+}
+
+fn parse_rss(url: &str, body: &str) -> Result<Vec<Item>> {
+	let mut rss: Rss = serde_xml_rs::from_str(body)?;
+	trace!(%url, ?rss, "parsed rss");
+
+	let items = take(&mut rss.channel.items);
+	info!(%url, ?rss, "found {} items in rss", items.len());
+
+	Ok(items)
 }
 
 #[derive(Clone, Debug, Deserialize)]
