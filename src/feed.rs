363e546dc70489ab9bc73978741d994a4ae733ca
Félix Saparelli <felix@passcod.name>
2021-09-17 19:48:39 +1200

    Parse feeds properly

--- src/feed.rs
+++ src/feed.rs
@@ -1,11 +1,10 @@
-use std::mem::take;
-
-use color_eyre::eyre::{eyre, Result};
+use color_eyre::eyre::Result;
 use futures::future::try_join_all;
+use itertools::Itertools;
 use mime::Mime;
-use serde::Deserialize;
+
 use sled::Tree;
-use tracing::{debug, info, trace, warn};
+use tracing::{debug, info, trace};
 
 use crate::cap::{fetch_cap, Cap};
 
@@ -32,30 +31,23 @@ pub async fn fetch_feed(cache: Tree, url: String) -> Result<Vec<Cap>> {
 	let media_type: Mime = content_type.parse()?;
 	trace!(%url, ?media_type, "decoded media type");
 
-	let body = resp.text().await?;
-	debug!(%url, chars=%body.chars().count(), "decoded body as text");
-	trace!(%url, body=%body, "decoded body");
+	let body = resp.bytes().await?;
+	debug!(%url, bytes=%body.len(), "read body");
+	trace!(%url, ?body, "full body");
 
-	let items = match (
-		media_type.type_(),
-		media_type.subtype().as_str(),
-		media_type.suffix().map(|s| s.as_str()),
-	) {
-		(mime::APPLICATION, "atom", Some("xml")) => {
-			parse_atom(&url, &body)?
-		},
-		(mime::APPLICATION, "rss", Some("xml")) => {
-			parse_rss(&url, &body)?
-		}
-		(mime::APPLICATION, "xml", None) => {
-			warn!(%url, ?media_type, "ambiguous media type, trying rss");
-			parse_rss(&url, &body).or_else(|err| {
-				warn!(%url, %err, "failed to parse as rss, trying atom");
-				parse_atom(&url, &body)
-			})?
-		}
-		_ => return Err(eyre!("unsupported media type: {}", media_type)),
-	};
+	let feed = feed_rs::parser::parse(&*body)?;
+	trace!(%url, ?feed, "parsed feed");
+	let items = feed
+		.entries
+		.into_iter()
+		.filter_map(|entry| {
+			entry.links.clone().first().map(|l| Item {
+				guid: entry.id,
+				link: l.href.to_string(),
+			})
+		})
+		.collect_vec();
+	debug!(%url, ?items, "extracted {} items", items.len());
 
 	let mut new = Vec::with_capacity(items.len());
 	for item in items {
@@ -91,79 +83,8 @@ pub async fn fetch_feed(cache: Tree, url: String) -> Result<Vec<Cap>> {
 	Ok(caps)
 }
 
-fn parse_rss(url: &str, body: &str) -> Result<Vec<Item>> {
-	let mut rss: Rss = serde_xml_rs::from_str(body)?;
-	trace!(%url, ?rss, "parsed rss");
-
-	let items: Vec<Item> = take(&mut rss.channel.items).into_iter().map(Item::from).collect();
-	info!(%url, "found {} items in rss", items.len());
-
-	Ok(items)
-}
-
-fn parse_atom(url: &str, body: &str) -> Result<Vec<Item>> {
-	let mut atom: Atom = serde_xml_rs::from_str(body)?;
-	trace!(%url, ?atom, "parsed atom");
-
-	let items: Vec<Item> = take(&mut atom.entries).into_iter().map(Item::from).collect();
-	info!(%url, "found {} items in atom", items.len());
-
-	Ok(items)
-}
-
-#[derive(Clone, Debug, Deserialize)]
-struct Rss {
-	pub channel: Channel,
-}
-
-#[derive(Clone, Debug, Deserialize)]
-#[serde(rename = "feed")]
-struct Atom {
-	#[serde(rename = "entry", default)]
-	pub entries: Vec<AtomEntry>,
-}
-
-#[derive(Clone, Debug, Deserialize)]
-struct Channel {
-	#[serde(rename = "item", default)]
-	pub items: Vec<RssItem>,
-}
-
-#[derive(Clone, Debug, PartialEq, Eq, Deserialize)]
+#[derive(Clone, Debug, PartialEq, Eq)]
 pub struct Item {
-	pub title: String,
 	pub guid: String,
 	pub link: String,
-}
-
-#[derive(Clone, Debug, PartialEq, Eq, Deserialize)]
-struct RssItem {
-	title: String,
-	guid: String,
-	link: String,
-}
-
-impl From<RssItem> for Item {
-	fn from(item: RssItem) -> Self {
-		Self { title: item.title, guid: item.guid, link: item.link }
-	}
-}
-
-#[derive(Clone, Debug, PartialEq, Eq, Deserialize)]
-struct AtomEntry {
-	title: String,
-	id: String,
-	#[serde(rename = "link")]
-	link: AtomLink,
-}
-
-impl From<AtomEntry> for Item {
-	fn from(entry: AtomEntry) -> Self {
-		Self { title: entry.title, guid: entry.id, link: entry.link.href }
-	}
-}
-
-#[derive(Clone, Debug, PartialEq, Eq, Deserialize)]
-struct AtomLink {
-	href: String,
 }
