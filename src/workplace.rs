0da9a6cd80885b9c4d7a6e5f9402a5bbc45166c7
Félix Saparelli <felix@passcod.name>
2021-09-17 15:47:29 +1200

    Send image to fb chat

--- src/workplace.rs
+++ src/workplace.rs
@@ -1,7 +1,10 @@
 use std::collections::HashMap;
 
 use color_eyre::eyre::{eyre, Result};
-use reqwest::Client;
+use reqwest::{
+	multipart::{Form, Part},
+	Client,
+};
 use serde::Serialize;
 use serde_json::Value;
 use tracing::trace;
@@ -9,7 +12,24 @@ use tracing::trace;
 use crate::output::{split_long_message, Out};
 
 pub async fn send(token: &str, thread_id: &str, out: &Out) -> Result<()> {
-	let mut out = out.clone();
+	let mut out = if let Out {
+		message,
+		image: Some(img),
+	} = out
+	{
+		let image_only = Out {
+			message: "".into(),
+			image: Some(img.clone()),
+		};
+		send_message(token, thread_id, &image_only).await?;
+		Out {
+			message: message.clone(),
+			image: None,
+		}
+	} else {
+		out.clone()
+	};
+
 	loop {
 		let (first, rest) = split_long_message(out, 2000, 280);
 		send_message(token, thread_id, &first).await?;
@@ -24,18 +44,39 @@ pub async fn send(token: &str, thread_id: &str, out: &Out) -> Result<()> {
 
 async fn send_message(token: &str, thread_id: &str, out: &Out) -> Result<()> {
 	let client = Client::new();
-	let resp = client
+	let req = client
 		.post("https://graph.facebook.com/me/messages")
-		.bearer_auth(token)
-		.header("Content-Type", "application/json")
-		.json(&MessageData {
-			message: Message::text(&out.message),
-			recipient: Recipient {
-				thread_key: thread_id.into(),
-			},
-		}) // TODO: image
-		.send()
-		.await?;
+		.bearer_auth(token);
+
+	let req = if let Some(ref imagedata) = out.image {
+		assert!(out.message.is_empty(), "cannot send both text and an image");
+
+		let part = Part::bytes(imagedata.clone())
+			.file_name("image.png")
+			.mime_str("image/png")?;
+		let form = Form::new()
+			.text(
+				"recipient",
+				serde_json::to_string(&Recipient {
+					thread_key: thread_id.into(),
+				})?,
+			)
+			.text("message", serde_json::to_string(&Message::image())?)
+			.part("filedata", part);
+
+		req.multipart(form)
+	} else {
+		req.header("Content-Type", "application/json")
+			.json(&MessageData {
+				message: Message::text(&out.message),
+				recipient: Recipient {
+					thread_key: thread_id.into(),
+				},
+			})
+	};
+
+	trace!(?req, "sending request");
+	let resp = req.send().await?;
 	trace!(?resp, "response from facebook");
 	let status = resp.status();
 
@@ -66,28 +107,25 @@ struct Recipient {
 
 #[derive(Clone, Debug, Default, Serialize)]
 struct Message {
+	#[serde(skip_serializing_if = "String::is_empty")]
 	text: String,
 
 	#[serde(skip_serializing_if = "Option::is_none")]
 	attachment: Option<Attachment>,
-
-	#[serde(skip_serializing_if = "Option::is_none")]
-	filedata: Option<String>,
 }
 
 impl Message {
 	fn text(text: impl Into<String>) -> Self {
 		Self {
 			text: text.into(),
-			..Self::default()
+			attachment: None,
 		}
 	}
 
-	fn text_and_image(text: impl Into<String>, image_ref: impl Into<String>) -> Self {
+	fn image() -> Self {
 		Self {
-			text: text.into(),
+			text: "".into(),
 			attachment: Some(Attachment::default()),
-			filedata: Some(image_ref.into()),
 		}
 	}
 }
